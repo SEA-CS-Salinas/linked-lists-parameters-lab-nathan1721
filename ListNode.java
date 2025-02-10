fcf5c93e292bb4e29b7b8ab67e4b29bbb6a6fbc3
nathan1721 <132502793+nathan1721@users.noreply.github.com>
2025-02-10 10:32:23 -0600

    Update ListNode.java

--- ListNode.java
+++ ListNode.java
@@ -1,42 +1,30 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
+public class ListNode implements Linkable {
+    private Comparable listNodeValue;
+    private ListNode nextListNode;
 
-//Name -
+    public ListNode() {
+        listNodeValue = null;
+        nextListNode = null;
+    }
 
-public class ListNode implements Linkable
-{
-	private Comparable listNodeValue;
-	private ListNode nextListNode;
+    public ListNode(Comparable value, ListNode next) {
+        listNodeValue = value;
+        nextListNode = next;
+    }
 
-	public ListNode()
-	{
-		listNodeValue = null;
-		nextListNode = null;
-	}
+    public Comparable getValue() {
+        return listNodeValue;
+    }
 
-	public ListNode(Comparable value, ListNode next)
-	{
-		listNodeValue=value;
-		nextListNode=next;
-	}
+    public ListNode getNext() {
+        return nextListNode;
+    }
 
-	public Comparable getValue()
-	{
-		return listNodeValue;
-	}
+    public void setValue(Comparable value) {
+        listNodeValue = value;
+    }
 
-	public ListNode getNext()
-	{
-	   return nextListNode;
-	}
-
-	public void setValue(Comparable value)
-	{
-		listNodeValue = value;
-	}
-
-	public void setNext(Linkable next)
-	{
-		nextListNode = (ListNode)next;
-	}
+    public void setNext(Linkable next) {
+        nextListNode = (ListNode) next;
+    }
 }
