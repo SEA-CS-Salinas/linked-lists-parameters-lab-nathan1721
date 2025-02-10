52c57ea38b464335cf4ecb5992f8185848158237
nathan1721 <132502793+nathan1721@users.noreply.github.com>
2025-02-10 10:45:13 -0600

    Update Linkable.java

--- Linkable.java
+++ Linkable.java
@@ -1,12 +1,10 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-
-//Name -
-
-public interface Linkable
-{
-   Comparable getValue();
-   Linkable getNext();
-   void setNext(Linkable next);
-   void setValue(Comparable value);
+/**
+ * Interface representing a linkable node in a linked list.
+ * Author: Nathan Carrillo
+ */
+public interface Linkable {
+    Comparable getValue(); // Returns the value of the node
+    Linkable getNext(); // Returns the next node in the list
+    void setNext(Linkable next); // Sets the next node in the list
+    void setValue(Comparable value); // Sets the value of the node
 }
