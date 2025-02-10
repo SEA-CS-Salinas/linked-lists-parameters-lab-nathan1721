63a12236f8d3bc7c0e8ed3c327fd6988bb44765c
nathan1721 <132502793+nathan1721@users.noreply.github.com>
2025-02-10 10:45:43 -0600

    Update ListNode.java

--- ListNode.java
+++ ListNode.java
@@ -1,30 +1,39 @@
-//-Nathan Carrillo-
+/**
+ * Represents a single node in a linked list.
+ * Author: Nathan Carrillo
+ */
 public class ListNode implements Linkable {
-    private Comparable listNodeValue;
-    private ListNode nextListNode;
+    private Comparable listNodeValue; // Value stored in the node
+    private ListNode nextListNode; // Reference to the next node
 
+    // Default constructor - initializes to null
     public ListNode() {
         listNodeValue = null;
         nextListNode = null;
     }
 
+    // Constructor that sets the value and next node
     public ListNode(Comparable value, ListNode next) {
         listNodeValue = value;
         nextListNode = next;
     }
 
+    // Returns the value stored in this node
     public Comparable getValue() {
         return listNodeValue;
     }
 
+    // Returns the next node in the list
     public ListNode getNext() {
         return nextListNode;
     }
 
+    // Sets the value of this node
     public void setValue(Comparable value) {
         listNodeValue = value;
     }
 
+    // Sets the next node in the list
     public void setNext(Linkable next) {
         nextListNode = (ListNode) next;
     }
